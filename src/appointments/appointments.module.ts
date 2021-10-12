0069e6f939fb1403d56419e5938a73e8e6862a26
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-12 23:28:37 +0700

    import prismaService

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsResolver } from './appointments.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [AppointmentsService, AppointmentsResolver]
+  providers: [AppointmentsService, AppointmentsResolver, PrismaService]
 })
 export class AppointmentsModule {}
