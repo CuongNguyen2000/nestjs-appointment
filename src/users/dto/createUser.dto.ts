b936fe79ce5efedde0686dd3b9dd5b09256bb03a
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-13 14:52:56 +0700

    define new dto validate

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,7 +1,12 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { NewUser } from '../../graphql';
+import { IsNotEmpty, IsString, IsEmail, IsEnum } from 'class-validator';
+import { NewUser, EnumUserRole } from '../../graphql';
 
 export class createUserDTO extends NewUser {
+    @IsEmail()
+    @IsString()
+    @IsNotEmpty()
+    readonly email: string;
+
     @IsString()
     @IsNotEmpty()
     readonly first_name: string;
@@ -12,4 +17,7 @@ export class createUserDTO extends NewUser {
 
     @IsString()
     readonly birthdate: string;
+
+    @IsEnum(EnumUserRole)
+    readonly role: EnumUserRole
 }
