005ade8f75f5d3232309b4b710604132e9b6b33a
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-13 22:30:19 +0700

    api CRUD appointment

--- src/appointments/appointments.resolver.ts
+++ src/appointments/appointments.resolver.ts
@@ -1,4 +1,28 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { AppointmentsService } from './appointments.service';
+import { createApptDTO } from './dto/createAppt.dto';
 
 @Resolver()
-export class AppointmentsResolver {}
+export class AppointmentsResolver {
+    constructor(private readonly apptService: AppointmentsService) {}
+
+    @Query('appointments')
+    async posts() {
+        return this.apptService.appointments();
+    }
+
+    @Query('appointment')
+    async post(@Args('id') args: string) {
+        return this.apptService.appointment(args);
+    }
+
+    @Mutation('createAppt')
+    async create(@Args('input') args: createApptDTO) {
+        return this.apptService.createAppt(args);
+    }
+
+    @Mutation('deleteAppt')
+    async delete(@Args('id') args: string) {
+        return this.apptService.deleteAppt(args);
+    }
+ }
--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,4 +1,90 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Appointment } from '@prisma/client';
+import { createApptDTO } from './dto/createAppt.dto';
+import { UserNotFoundException, ApptNotFoundException } from '../exceptions/NotFound.exception';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaError } from '../utils/prismaError';
 
 @Injectable()
-export class AppointmentsService {}
+export class AppointmentsService {
+    constructor(private prisma: PrismaService) { }
+
+    // Get a single appointment
+    async appointment(id: string): Promise<Appointment | null> {
+        const appt = await this.prisma.appointment.findUnique({
+            where: {
+                id: parseInt(id),
+            },
+            include: {
+                user: true, // Return all fields
+            },
+        });
+
+        if (!appt) throw new ApptNotFoundException(parseInt(id));
+
+        return appt;
+    }
+
+    // Get multiple posts
+    async appointments(): Promise<Appointment[]> {
+        return this.prisma.appointment.findMany({
+            include: {
+                user: true, // Return all fields
+            },
+        });
+    }
+
+    // Create an appointment
+    async createAppt(input: createApptDTO): Promise<Appointment> {
+
+        const today = new Date();
+        const userExist = await this.prisma.user.findUnique({
+            where: {
+                id: parseInt(input.user),
+            },
+        });
+
+        if (!userExist) throw new UserNotFoundException(parseInt(input.user));
+
+
+        const newAppt = await this.prisma.appointment.create({
+            data: {
+                ...input,
+                user: {
+                    connect: {
+                        id: userExist.id,
+                    },
+                },
+            },
+            include: {
+                user: true, // Return all fields
+            },
+        });
+        return newAppt;
+    }
+
+    // delete an appointment
+    async deleteAppt(id: string): Promise<Appointment> {
+
+        try {
+            const deleteAppt = await this.prisma.appointment.delete({
+                where: {
+                    id: parseInt(id),
+                },
+                include: {
+                    user: true, // Return all fields
+                },
+            });
+            return deleteAppt;
+        } catch (error) {
+            if (
+                error instanceof PrismaClientKnownRequestError &&
+                error.code === PrismaError.RecordDoesNotExist
+            ) {
+                throw new ApptNotFoundException(parseInt(id));
+            }
+            throw error;
+        }
+    }
+}
