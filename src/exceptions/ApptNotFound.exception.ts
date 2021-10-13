61b9a945464fa3c29087b911a77a354b3ce474f5
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-13 16:12:48 +0700

    update folder

--- src/exceptions/ApptNotFound.exception.ts
+++
@@ -1,7 +0,0 @@
-import { NotFoundException } from '@nestjs/common';
-
-export class ApptNotFoundException extends NotFoundException {
-    constructor(apptId: number) {
-        super(`Appointment with id ${apptId} not found`);
-    }
-}
