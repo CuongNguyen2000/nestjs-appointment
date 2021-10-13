005f7b6a65597d5d40b3275373f4075e11e4c51b
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-13 23:08:20 +0700

    update api create appt

--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Appointment } from '@prisma/client';
 import { createApptDTO } from './dto/createAppt.dto';
@@ -37,7 +37,6 @@ export class AppointmentsService {
 
     // Create an appointment
     async createAppt(input: createApptDTO): Promise<Appointment> {
-
         const today = new Date();
         const userExist = await this.prisma.user.findUnique({
             where: {
@@ -47,6 +46,16 @@ export class AppointmentsService {
 
         if (!userExist) throw new UserNotFoundException(parseInt(input.user));
 
+        if (Date.parse(input.start_date) < today.valueOf()) {
+            throw new HttpException('Start date must be greater than or equal to today', HttpStatus.BAD_REQUEST);
+        } 
+        
+        if (Date.parse(input.start_date) < Date.parse(input.end_date)) {
+            throw new HttpException('End date must be less than start date', HttpStatus.BAD_REQUEST);
+        }
+
+        console.log(Date.parse(input.start_date))
+        console.log(Date.parse(input.end_date))
 
         const newAppt = await this.prisma.appointment.create({
             data: {
